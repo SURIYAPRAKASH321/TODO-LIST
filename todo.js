6db150dc5fabbea85d8ffbbc23c09abec7f1662c
SURIYAPRAKASH321 <140058588+SURIYAPRAKASH321@users.noreply.github.com>
2023-08-02 18:42:49 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -7,25 +7,20 @@ let list = [];
 button.addEventListener('click', () => {
     list.push(input.value);
     addfun(input.value);
-    input.value = ''; // Clear the input field after adding the task
+    input.value = ''; 
 });
 
 function addfun(x) {
     let p = document.createElement('p');
     p.innerText = x;
     taskList.appendChild(p);
-
-    // Add a click event listener to each task item (paragraph)
     p.addEventListener('click', () => {
         removeTask(p);
     });
 }
 
 function removeTask(taskElement) {
-    // Find the index of the task in the list
     let index = list.indexOf(taskElement.innerText);
-
-    // Remove the task from the list and the taskList
     if (index !== -1) {
         list.splice(index, 1);
         taskList.removeChild(taskElement);
